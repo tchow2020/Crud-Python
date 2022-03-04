8fd811c25579ac336e2601ae65a29abb1221817b
tchow2020 <lgmds2017@outlook.com>
2022-03-04 14:24:05 -0300

    Concluído

--- mysqlpython.py
+++ mysqlpython.py
@@ -4,14 +4,14 @@ from django.db import DEFAULT_DB_ALIAS
 import mysql.connector
 import datetime
 
-data = datetime.date.today()
+data = datetime.datetime.today()
 
 #conexão com o banco
 banco = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="desafio"
+    database="desafio",
 )
 
 database = banco.cursor()
@@ -19,27 +19,40 @@ database = banco.cursor()
 #criando a tabela
 #database.execute('CREATE TABLE user (user_id int(50) not null auto_increment primary key,name varchar(35),email varchar(50),password varchar(50))')
 
-#crud insert
-database.execute("INSERT INTO user (name, email, password) VALUES(%s,%s,%s)" , ('Teste', 'teste@outlook.com', '123456'))
-banco.commit()
-print(database.rowcount, "dados inseridos", data)
+def Insert():
+    
+    database = banco.cursor()  
+    
+    database.execute("INSERT INTO user (name, email, password, createdAt) VALUES(%s,%s,%s,%s)" , ('Teste2', 'teste@outlook.com', '123456', data))
+    banco.commit()
+    print(database.rowcount, "dados inseridos")
 
-#crud Select
-#database.execute('SELECT * FROM user')
-#result = database.fetchall()
-#for name in result:
-#    print(name)
-#print(database.rowcount, "resultados encontrados")
 
-#crud Delete
-#dell = "DELETE FROM user WHERE name='Gustavo' "
-#database.execute(dell)
-#banco.commit()
-#print(database.rowcount, "coluna deletada")
+def Select():
+     
+    database = banco.cursor()  
+     
+    database.execute('SELECT * FROM user')
+    result = database.fetchall()
+    for name in result:
+        print(name)
+    print(database.rowcount, "resultados encontrados")
 
-#crud Update
-#att = "UPDATE user SET name = 'Gustavo' WHERE name = 'teste' "
-#database.execute(att)
-#banco.commit()
-#print(database.rowcount, "Atualizado com sucesso")
 
+def Delete():
+    
+    dell = "DELETE FROM user WHERE user_id in () "
+    database.execute(dell)
+    banco.commit()
+    print(database.rowcount, "coluna deletada")
+    
+
+def Update():
+    
+    att = f"UPDATE user SET name =  'Cleiton', password = '123Cleitin', email = 'nadinho@gmail.com', updatedAT = '{data}'  WHERE user_id =  26 "
+    database.execute(att)
+    banco.commit()
+    print(database.rowcount, "Atualizado com sucesso")
+
+
+Update()
